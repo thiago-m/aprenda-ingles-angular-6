e3251b77e3e641d174ea75250b14259f308bf4ca
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2019-12-31 18:28:24 -0300

    Verifgicando resposta e evoluindo a rodada

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,10 +12,32 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = 'Traduza a frase:'
   public frases: Frase[] = FRASES
+  public resposta: string
 
-  constructor() { }
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+  }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(res: Event): void {
+    this.resposta = (<HTMLInputElement>res.target).value
+  }
+
+  public verificarResposta(): void {
+    if(this.resposta.toUpperCase().trim() === this.rodadaFrase.frasePtBr.toUpperCase()) {
+      alert('Acertou')  
+      // Trocar pergunta da rotada
+      this.rodada++    
+      // Atualiza a frase
+      this.rodadaFrase = this.frases[this.rodada]
+    } else {
+      alert('Errou')
+    }
   }
 
 }
